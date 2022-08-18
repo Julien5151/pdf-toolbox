3c42ab5dca649fd7ab4e7573275d6e6acb4a6f41
Julien CLAUDEL <julienclaudel51@outlook.com>
2022-08-18 11:46:02 +0200

    Added basic swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+    // Create nest application
     const app = await NestFactory.create(AppModule);
+    // Swagger configuration
+    const config = new DocumentBuilder()
+        .setTitle('PdfToolbox API')
+        .setDescription('Toolbox for manipulating PDF files')
+        .setVersion('1.0')
+        .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api-docs', app, document);
+    // Start application
     await app.listen(3000);
 }
 bootstrap();
