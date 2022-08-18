ce760f8a970c7178fc7b18368d803f29501dfcfa
Julien CLAUDEL <julienclaudel51@outlook.com>
2022-08-18 15:41:32 +0200

    Added pdf merging

--- src/tools/tools.controller.ts
+++ src/tools/tools.controller.ts
@@ -1,11 +1,13 @@
 import {
     Controller,
     Post,
+    StreamableFile,
     UploadedFiles,
     UseInterceptors,
 } from '@nestjs/common';
 import { AnyFilesInterceptor } from '@nestjs/platform-express';
 import { ApiBody, ApiConsumes, ApiTags } from '@nestjs/swagger';
+import { PDFDocument } from 'pdf-lib';
 
 @Controller('tools')
 @ApiTags('Tools')
@@ -27,8 +29,27 @@ export class ToolsController {
         },
     })
     @UseInterceptors(AnyFilesInterceptor())
-    uploadFile(@UploadedFiles() files: Array<Express.Multer.File>) {
-        console.log(files);
-        return 'Files merged';
+    async uploadFile(
+        @UploadedFiles() files: Array<Express.Multer.File>,
+    ): Promise<StreamableFile> {
+        // Extract file buffers
+        const pdfsToMerge = files.map((file) => file.buffer);
+        // Init empty document
+        const mergedPdf = await PDFDocument.create();
+        // Add pages from each file
+        for (const pdfBytes of pdfsToMerge) {
+            const pdf = await PDFDocument.load(pdfBytes);
+            const copiedPages = await mergedPdf.copyPages(
+                pdf,
+                pdf.getPageIndices(),
+            );
+            copiedPages.forEach((page) => {
+                mergedPdf.addPage(page);
+            });
+        }
+        // Save merged file
+        const buffer = await mergedPdf.save();
+        // Return streamable file
+        return new StreamableFile(buffer, { type: 'application/pdf' });
     }
 }
